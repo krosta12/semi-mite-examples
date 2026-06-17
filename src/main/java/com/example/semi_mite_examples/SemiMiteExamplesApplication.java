d7de4730e74ba5e51d3d4d4272229fb4326bee92
krosta12 <kostailusenko911@gmail.com>
2026-06-17 14:06:09 +0300

    implemented a semi-mite dependency (local repo)

--- src/main/java/com/example/semi_mite_examples/SemiMiteExamplesApplication.java
+++ src/main/java/com/example/semi_mite_examples/SemiMiteExamplesApplication.java
@@ -1,9 +1,11 @@
 package com.example.semi_mite_examples;
 
+import org.example.client.EnableMiteClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMiteClients
 public class SemiMiteExamplesApplication {
 
 	public static void main(String[] args) {
