c714e194278558afa542debe8ff4fc9ff8e2bfcc
krosta12 <kostailusenko911@gmail.com>
2026-06-17 13:12:37 +0300

    init main project

---
+++ src/main/java/com/example/semi_mite_examples/SemiMiteExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.semi_mite_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiMiteExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiMiteExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/semi_mite_examples/SemiMiteExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semi_mite_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiMiteExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
